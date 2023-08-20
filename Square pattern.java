1b864ce294aedc93cfd4e212d26b4a3bcd603bd9
AishwaryaSrinath18 <141122047+AishwaryaSrinath18@users.noreply.github.com>
2023-08-20 15:18:40 +0530

    To print Square Pattern

---
+++ Square pattern.java
@@ -0,0 +1,11 @@
+public class Print {
+    public static void main (String arg[]){
+        for (int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+}
