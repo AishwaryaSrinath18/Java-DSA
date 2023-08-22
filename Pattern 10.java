5f5d1d5fe83c76c4d06d67d0e2171c27a85c1b72
AishwaryaSrinath18 <141122047+AishwaryaSrinath18@users.noreply.github.com>
2023-08-22 19:24:23 +0530

    Create Pattern 10.java
    Pattern 10 Half Diamond

---
+++ Pattern 10.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    void printTriangle(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print("* ");
+            }
+        System.out.println();
+        }
+          for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print("* ");
+            }
+        System.out.println();
+    }
+}
+}
