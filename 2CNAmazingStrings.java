af00301858e4dd8ef4ecc60179300aaf9b332a81
AishwaryaSrinath18 <141122047+AishwaryaSrinath18@users.noreply.github.com>
2024-02-22 11:31:51 +0530

    2CNAmazingStrings.java

--- 2CNAmazingStrings.java
+++ 2CNAmazingStrings.java
@@ -1,25 +1,4 @@
-import java.util.Arrays;
-public class Solution {
-// Given 3 strings whether 3rd contains both 1 and 2 and not any extra in it
-	
-	public static void answer(String s1,String s2,String s3)
-	{	String combined = s1.concat(s2);
-	    char str3[] = s3.toCharArray();
-		char comStr[] = combined.toCharArray();
-		Arrays.sort(comStr);
-		Arrays.sort(str3);
-		if(Arrays.equals(str3, comStr)){
-			System.out.println("YES");
-		}
-		else{
-			System.out.println("NO");
-		}
-	// Write your code here
-	}
-}
-
-
-Another method:
+First New method:
 import java.util.Arrays;
 public class Solution {
 
@@ -50,3 +29,28 @@ public class Solution {
 
 	
 }
+
+
+Second Method:
+
+import java.util.Arrays;
+public class Solution {
+// Given 3 strings whether 3rd contains both 1 and 2 and not any extra in it
+	
+	public static void answer(String s1,String s2,String s3)
+	{	String combined = s1.concat(s2);
+	    char str3[] = s3.toCharArray();
+		char comStr[] = combined.toCharArray();
+		Arrays.sort(comStr);
+		Arrays.sort(str3);
+		if(Arrays.equals(str3, comStr)){
+			System.out.println("YES");
+		}
+		else{
+			System.out.println("NO");
+		}
+	// Write your code here
+	}
+}
+
+
