4d5381e9e11c832f35656d2117816078360035ca
AishwaryaSrinath18 <141122047+AishwaryaSrinath18@users.noreply.github.com>
2024-03-16 21:34:16 +0530

    58LDPClimingStairs.java

---
+++ 58LDPClimingStairs.java
@@ -0,0 +1,17 @@
+// Problem is resembling fibonacci sequence 1,1,2,3,5,8,13,...
+// here we use dp logic to solve the problem
+// bottom to top model, Time and space complexity: O(n)
+// using recursion will have time limit exceeding problem 
+// O(2^n) complexity
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        // array to store no of ways to reach steps
+        // no of ways to reach ground is 1
+        dp[0] = 1; // ground
+        dp[1] = 1; // ways to reach first step
+        for(int i=2; i<dp.length; i++) dp[i] = dp[i-1] + dp[i-2];
+        // fibonacci series
+        return dp[dp.length -1];
+    }
+}
