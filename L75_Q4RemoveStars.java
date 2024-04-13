230b52b5ebba137c4c566c00b72740023697e899
AishwaryaSrinath18 <141122047+AishwaryaSrinath18@users.noreply.github.com>
2024-04-13 17:18:41 +0530

    L75_Q4RemoveStars.java

---
+++ L75_Q4RemoveStars.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeStars(String s) {
+       String removed = "";
+        int index = 0; // stack index
+        char[] stack = new char[s.length()];
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) != '*'){
+                stack[index] = s.charAt(i);
+                index++;
+            }
+            else{
+                index--; // moving index to the left one and making it null
+                stack[index] = '\u0000'; // null character
+            }
+        }
+        for(int i=0; i<stack.length; i++){
+            if(stack[i] != '\u0000') removed = removed + stack[i];
+        }
+        return removed;
+    }
+}
